dcb3d6cafa0948a65581ea28262f43fefb342945
ELG1N <114599200+ELG1N@users.noreply.github.com>
2026-03-23 17:24:34 -0600

    added logic for Ket-door interaction
    added logic for Ket-door interaction

---
+++ scripts/key_door.gd
@@ -0,0 +1,10 @@
+
+extends Area2D
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		if body.has_key == true:
+			$"..".open()
