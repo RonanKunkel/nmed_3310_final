f17b8714d4c414aa8748299cea318ab11fe1b7f8
andriyzynyuk <andriyzynyuk@gmail.com>
2026-03-23 15:28:37 -0600

    Added lever

---
+++ scripts/lever.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var playerInRange = false;
+var leverActivated = false;
+
+signal lever_toggled(state)
+
+func _ready() -> void:
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
+
+func _process(delta: float) -> void:
+	if playerInRange and Input.is_action_just_pressed("interact"):
+		toggle_lever()
+		
+func toggle_lever() -> void:
+	leverActivated = !leverActivated
+	emit_signal("lever_toggled", leverActivated)
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		playerInRange = true
+		
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		playerInRange = false
+	
