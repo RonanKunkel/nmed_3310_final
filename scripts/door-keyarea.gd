0ef3768abe5e5877616cc3bfec7ae45c239efbce
ELG1N <114599200+ELG1N@users.noreply.github.com>
2026-03-24 12:18:30 -0600

    updated the door and key logic
    Now the area2d for the key recognition has a direct connection to the door itself, structure will not matter for the logic hopefully

--- scripts/door-keyarea.gd
+++ scripts/door-keyarea.gd
@@ -1,5 +1,6 @@
 
 extends Area2D
+@export var key_door: AnimatableBody2D
 
 func _ready():
 	body_entered.connect(_on_body_entered)
@@ -7,4 +8,4 @@ func _ready():
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		if body.has_key == true:
-			$"key_door".open()
+			key_door.open()
