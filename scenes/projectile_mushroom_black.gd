4d1d4a365ca3470ca4c39e33fb7207dd2366b004
RonanKunkel <ronankunkel@gmail.com>
2026-03-31 03:26:27 -0600

    I should go to bed

---
+++ scenes/projectile_mushroom_black.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var speed = 75
+var direction = Vector2.LEFT
+@onready var light = $PointLight2D
+
+func _physics_process(delta):
+	# Move the projectile in the assigned direction
+	position += direction * speed * delta
+
+# Connect the body_entered signal to handle collisions
+func _on_body_entered(body: Node) -> void:
+	if body.has_method("die"):
+		light.enabled = false
+		body.die()
+		queue_free()
+	else:
+		light.enabled = false
+		queue_free()
