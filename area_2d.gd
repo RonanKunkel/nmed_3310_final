1c3a367e154c2f4d918a74f87afd1c7d45a03cf8
ELG1N <114599200+ELG1N@users.noreply.github.com>
2026-03-31 19:58:33 -0600

    l

--- area_2d.gd
+++ area_2d.gd
@@ -4,3 +4,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.die()
+
+
+func _on_endgame_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
