585c8c29643a3f293521266b1da3cb7ce88838d0
Maksym Makarov <114599200+ELG1N@users.noreply.github.com>
2026-04-08 15:54:54 -0600

    added timed/not timer runs with main menu UI for it

--- autoload.gd
+++ autoload.gd
@@ -2,9 +2,11 @@ extends Node
 
 var time_elapsed: float = 0.0
 var timer_running: bool = false
+var timed_mode: bool = false  # set this before starting a run
 
 func start() -> void:
-	timer_running = true
+	if timed_mode:
+		timer_running = true
 
 func stop() -> void:
 	timer_running = false
@@ -14,10 +16,12 @@ func reset() -> void:
 	timer_running = false
 
 func _process(delta: float) -> void:
-	if timer_running:
+	if timer_running and timed_mode:
 		time_elapsed += delta
 
 func get_formatted_time() -> String:
+	if not timed_mode:
+		return ""
 	var minutes := int(time_elapsed / 60)
 	var seconds := int(fmod(time_elapsed, 60))
 	var msec := int(fmod(time_elapsed, 1) * 100)
--- scenes/main_menucanvas.gd
+++ scenes/main_menucanvas.gd
@@ -3,12 +3,15 @@ extends CanvasLayer
 @onready var main_buttons = $VBoxContainer
 @onready var level_selector = $LevelSelector
 @onready var credits = $Credits
-
+@onready var timer = $timer
 func _ready():
 	level_selector.hide()
 	credits.hide()
+	timer.hide()
 	
 	$VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+	$timer/VBoxContainer3/timed.pressed.connect(_on_yes_pressed)
+	$timer/VBoxContainer3/untimed.pressed.connect(_on_no_pressed)
 	$VBoxContainer/TutorialButton.pressed.connect(_on_tutorial_pressed)
 	$VBoxContainer/LevelSelectorButton.pressed.connect(_on_level_selector_pressed)
 	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
@@ -17,9 +20,11 @@ func _ready():
 	$LevelSelector/VBoxContainer/Level2.pressed.connect(_on_level2_pressed)
 	$LevelSelector/VBoxContainer/Level3.pressed.connect(_on_level3_pressed)
 	$LevelSelector/VBoxContainer/BackButton.pressed.connect(_on_back_pressed)
+	$timer/VBoxContainer3/BackButton.pressed.connect(_on_back_pressed)
 	$Credits/BackButton.pressed.connect(_on_back_pressed)
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/LVL1.tscn")
+	main_buttons.hide()
+	timer.show()
 
 func _on_tutorial_pressed():
 	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
@@ -31,7 +36,17 @@ func _on_level_selector_pressed():
 func _on_credits_pressed():
 	main_buttons.hide()
 	credits.show()
+	
+func _on_yes_pressed():
+	SpeedRunTimer.timed_mode = true
+	SpeedRunTimer.reset()
+	SpeedRunTimer.start()
+	get_tree().change_scene_to_file("res://scenes/LVL1.tscn")
 
+func _on_no_pressed():
+	SpeedRunTimer.timed_mode = false
+	SpeedRunTimer.reset()
+	get_tree().change_scene_to_file("res://scenes/LVL1.tscn")
 
 func _on_level1_pressed():
 	get_tree().change_scene_to_file("res://scenes/LVL1.tscn")
@@ -45,6 +60,7 @@ func _on_level3_pressed():
 func _on_back_pressed():
 	level_selector.hide()
 	credits.hide()
+	timer.hide()
 	main_buttons.show()
 
 func _on_quit_pressed():
